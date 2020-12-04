3d45425dabeee35c4fe23e16f30d59ee51c366bf
Cerek Hillen <chillen@lyft.com>
2020-12-04 01:43:19 -0500

    attempt to do the thing

--- src/problem_01.rs
+++ src/problem_01.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
+use std::io::prelude::*;
 use std::io::BufReader;
 use std::io::Result;
-use std::io::prelude::*;
 use std::iter::FromIterator;
 use std::path::Path;
 
@@ -17,7 +17,7 @@ pub fn part2() -> io::Result<u32> {
 
 fn part_impl(term_count: usize) -> io::Result<u32> {
     let terms = load_terms()?;
-    let values = find_summing_terms(terms, term_count, 2020);
+    let values = find_summing_terms(&terms, term_count, 2020);
 
     let mut i = 1;
     for value in values.iter() {
@@ -41,8 +41,36 @@ fn load_terms() -> Result<Vec<u32>> {
     Ok(terms)
 }
 
-fn find_summing_terms(terms: Vec<u32>, term_count: usize, target: u32) -> Vec<u32> {
-    // TODO: do this here!
+fn find_summing_terms(terms: &Vec<u32>, term_count: usize, target: u32) -> Vec<u32> {
+    // NOTE: this has really bad worst case complexity, but works in practice because it
+    // continuously filters out all of the terms that are too big to be feasible.
+    //
+    // i'm here to futz around with rust, not make good algorithms, come on!
+    if term_count == 1 {
+        for term in terms.iter() {
+            if *term == target {
+                return vec![*term];
+            }
+        }
+        return vec![];
+    }
+
+    let mut reduced_terms = HashMap::new();
+    for term in terms.iter().copied() {
+        if term > target {
+            continue;
+        }
+        reduced_terms.insert(term, target - term);
+    }
+
+    for (value, reduced) in reduced_terms.iter() {
+        let mut summing_terms = find_summing_terms(terms, term_count - 1, *reduced);
+        if summing_terms.len() > 0 {
+            summing_terms.push(*value);
+            return summing_terms;
+        }
+    }
+
     vec![]
 }
 
@@ -56,7 +84,7 @@ mod tests {
     #[test]
     fn test_two_terms() {
         let terms = Vec::from_iter(TERMS.iter().copied());
-        let output = find_summing_terms(terms, 2, TARGET);
+        let output = find_summing_terms(&terms, 2, TARGET);
         let product: u32 = output.iter().product();
 
         assert_eq!(output.len(), 2);
@@ -66,7 +94,7 @@ mod tests {
     #[test]
     fn test_three_terms() {
         let terms = Vec::from_iter(TERMS.iter().copied());
-        let output = find_summing_terms(terms, 3, TARGET);
+        let output = find_summing_terms(&terms, 3, TARGET);
         let product: u32 = output.iter().product();
 
         assert_eq!(output.len(), 3);
