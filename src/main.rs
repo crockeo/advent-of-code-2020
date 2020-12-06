1755012de7a1e966ff80073dd2103f276be185aa
Cerek Hillen <chillen@lyft.com>
2020-12-05 19:11:27 -0500

    part of problem 5...need data which i don't have

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod problem_01;
 pub mod problem_02;
 pub mod problem_03;
 pub mod problem_04;
+pub mod problem_05;
 
 use std::io;
 
---
+++ src/problem_05.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+pub fn part1() -> io::Result<i32> {
+    Ok(0)
+}
+
+fn find_seat(rows: i32, cols: i32, locator: &str) -> Option<(i32, i32)> {
+    let mut row_min = 0;
+    let mut row_max = rows;
+    let mut col_min = 0;
+    let mut col_max = cols;
+
+    for c in locator.chars() {
+        match c {
+            'F' => row_max = (row_min + row_max) / 2,
+            'B' => row_min = (row_min + row_max) / 2,
+            'L' => col_max = (col_min + col_max) / 2,
+            'R' => col_min = (col_min + col_max) / 2,
+            _ => return None,
+        }
+    }
+
+    if row_min == row_max - 1 && col_min == col_max - 1 {
+        Some((row_min, col_min))
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_find_seat() {
+        let locator = "FBFBBFFRLR";
+        let location = find_seat(128, 8, locator).unwrap();
+        assert_eq!(location, (44, 5));
+    }
+
+    #[test]
+    fn test_find_seat_failure() {
+        let locators = vec![
+            "fake locator", // poorly structured
+            "FBBFRLR", // incomplete rows
+            "FBFBBFFRL" // incomplete columns
+        ];
+        for locator in locators.into_iter() {
+            assert_eq!(find_seat(128, 8, locator), None);
+        }
+    }
+}
